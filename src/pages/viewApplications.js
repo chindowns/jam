4116c631a825403661b906937a8392af2f80c911
Michael Downs <michael@chindowns.com>
2020-07-30 21:07:37 -0700

    Sort Application View by Descending Order making declined last

--- src/pages/viewApplications.js
+++ src/pages/viewApplications.js
@@ -19,9 +19,33 @@ export default () => {
         })
     }
 
+    function compareValues(stage, order = 'desc') {
+        return function innerSort(a, b) {
+          if (!a.hasOwnProperty(stage) || !b.hasOwnProperty(stage)) {
+            // property doesn't exist on either object
+            return 0;
+          }
+      
+          const varA = (typeof a[stage] === 'string')
+            ? a[stage].toUpperCase() : a[stage];
+          const varB = (typeof b[stage] === 'string')
+            ? b[stage].toUpperCase() : b[stage];
+      
+          let comparison = 0;
+          if (varA > varB) {
+            comparison = 1;
+          } else if (varA < varB) {
+            comparison = -1;
+          }
+          return (
+            (order === 'desc') ? (comparison * -1) : comparison
+          );
+        };
+      }
+
     return (
         <Row style={{ margin: '15px 15px' }}>
-            {applications.map((application, idx) => (<Card application={application} key={idx} />))}
+            {applications.sort(compareValues('stage')).map((application, idx) => (<Card application={application} key={idx} />))}
         </Row>
     )
 }

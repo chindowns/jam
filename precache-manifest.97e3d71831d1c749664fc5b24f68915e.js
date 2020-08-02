1b62c115a0173f8f96e9eeddec290c5874d95a98
ChinDowns <webdev@chindowns.com>
2020-08-01 17:06:49 -0700

    Updates

---
+++ precache-manifest.97e3d71831d1c749664fc5b24f68915e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1eb9063142bea697e3d329f8927513f",
+    "url": "/jam/index.html"
+  },
+  {
+    "revision": "6e7102528dbd4293bd94",
+    "url": "/jam/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "350f76d674bf202199f4",
+    "url": "/jam/static/css/main.6ab6d184.chunk.css"
+  },
+  {
+    "revision": "6e7102528dbd4293bd94",
+    "url": "/jam/static/js/2.c7d933f0.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/jam/static/js/2.c7d933f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350f76d674bf202199f4",
+    "url": "/jam/static/js/main.0cebb62a.chunk.js"
+  },
+  {
+    "revision": "281af16a49c7b8550dc2",
+    "url": "/jam/static/js/runtime-main.37121efb.js"
+  },
+  {
+    "revision": "1c0708a88937e5970cf48c40e7db6a7f",
+    "url": "/jam/static/media/core-background.1c0708a8.jpg"
+  }
+]);
